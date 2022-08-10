0546756cba2ba5d57b1f706f509af6f45449f0f8
tzulinho <tzulinho@github.com>
2022-08-10 21:38:59 +0800

    fix: GA isn't working

--- src/container/App.js
+++ src/container/App.js
@@ -5,13 +5,11 @@ import Divider from '@mui/material/Divider'
 
 import ReactGA from 'react-ga'
 
-const trackingId = 'G-YMTQHQB60B'
-const appURL = 'https://mycatsfitness.netlify.app/'
-ReactGA.initialize(trackingId)
-ReactGA.send(window.location.pathname + window.location.search)
-ReactGA.send(appURL)
-
 function App() {
+  const trackingId = 'G-YMTQHQB60B'
+  ReactGA.initialize(trackingId)
+  ReactGA.pageview(window.location.pathname + window.location.search)
+
   return (
     <div className="App">
       <CaloriesForm />
