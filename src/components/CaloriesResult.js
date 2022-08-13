864eda351f612a6b60ea746cb2286f875f13501a
Shane Ho <shanetzulinho@gmail.com>
2022-08-14 01:12:34 +0800

    refactor: separate Calories component to Form and Result
    Related to #42

---
+++ src/components/CaloriesResult.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { showMessage } from '../components/caloriesForm.helpers'
+import Alert from '@mui/material/Alert'
+import Stack from '@mui/material/Stack'
+
+const CaloriesResult = ({ lowerUpperBound }) => {
+  const { lowerBound, upperBound } = lowerUpperBound
+  const caloriesMessage = showMessage(lowerBound, upperBound)
+
+  return (
+    <div>
+      <Stack sx={{ width: '100%' }} spacing={1} my={2}>
+        <Alert severity="info">
+          Estimated daily calories (估計每日攝取卡路里): {caloriesMessage}
+        </Alert>
+      </Stack>
+    </div>
+  )
+}
+
+export default CaloriesResult
--- src/container/App.js
+++ src/container/App.js
@@ -1,6 +1,11 @@
+import { useState } from 'react'
+
 import './App.css'
 import CaloriesForm from '../components/CaloriesForm'
+import CaloriesResult from '../components/CaloriesResult'
 import NutrientForm from '../components/NutrientForm'
+
+import Container from '@mui/material/Container'
 import Divider from '@mui/material/Divider'
 
 import ReactGA from 'react-ga4'
@@ -10,11 +15,23 @@ function App() {
   ReactGA.initialize(trackingId)
   ReactGA.send({ hitType: 'pageview', page: 'https://mycatsfitness.netlify.app/' })
 
+  const [lowerUpperBound, setLowerUpperBound] = useState({
+    lowerBound: 0,
+    upperBound: 0,
+  })
+
+  const getLowerUpperBound = ({ lowerBound, upperBound }) => {
+    setLowerUpperBound({ lowerBound: lowerBound, upperBound: upperBound })
+  }
+
   return (
     <div className="App">
-      <CaloriesForm />
-      <Divider variant="middle" />
-      <NutrientForm />
+      <Container maxWidth="md">
+        <CaloriesForm getLowerUpperBound={getLowerUpperBound} />
+        <CaloriesResult lowerUpperBound={lowerUpperBound} />
+        <Divider variant="middle" />
+        <NutrientForm />
+      </Container>
     </div>
   )
 }
