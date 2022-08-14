490ba835fe219c0aba99abf018525e211d0530ef
tzulinho <tzulinho@github.com>
2022-08-14 21:54:19 +0800

    refactor: add inputProps attribute in InputField

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -2,16 +2,16 @@ import React from 'react'
 import TextField from '@mui/material/TextField'
 
 const InputField = React.forwardRef(
-  ({ label, type, name, value, onChange, InputProps, helperText, error }, ref) => {
+  ({ label, type, name, value, onChange, error, helperText }, ref) => {
     return (
       <TextField
         variant="outlined"
         fullWidth
+        InputProps={{ inputProps: { min: 0.1, step: 0.1 } }}
         label={label}
         type={type}
         name={name}
         value={value}
-        InputProps={InputProps}
         onChange={onChange}
         error={error}
         helperText={helperText}
